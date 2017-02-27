3d3dd1996319b09e6adf87a65074e90ed5801913
Oli Mortimer <olimortimer@gmail.com>
2017-02-27 10:40:49 +0000

    Added Channel Islands to countries and zones

--- install/database.sql
+++ install/database.sql
@@ -320,7 +320,8 @@ INSERT INTO `gc_countries` (`id`, `sequence`, `name`, `iso_code_2`, `iso_code_3`
 ('236', '235', 'Yugoslavia', 'YU', 'YUG', '', '1', '1', '0.00'),
 ('237', '236', 'Democratic Republic of Congo', 'CD', 'COD', '', '1', '1', '0.00'),
 ('238', '237', 'Zambia', 'ZM', 'ZMB', '', '0', '1', '0.00'),
-('239', '238', 'Zimbabwe', 'ZW', 'ZWE', '', '0', '1', '0.00');
+('239', '238', 'Zimbabwe', 'ZW', 'ZWE', '', '0', '1', '0.00'),
+('240', '0', 'Channel Islands', 'GB', 'GBR', '', '0', '1', '0.00');
 
 CREATE TABLE `gc_country_zone_areas` (
   `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
@@ -4276,7 +4277,9 @@ INSERT INTO `gc_country_zones` (`id`, `country_id`, `code`, `name`, `status`, `t
 ('3953', '222', 'LDY', 'County Londonderry', '1', '0.00'),
 ('3954', '222', 'TYR', 'County Tyrone', '1', '0.00'),
 ('3955', '222', 'CMA', 'Cumbria', '1', '0.00'),
-('3956', '138', 'AG', 'Aguascalientes', '1', '0.00');
+('3956', '138', 'AG', 'Aguascalientes', '1', '0.00'),
+('3956', '240', 'GE', 'Guernsey', '1', '0.00'),
+('3956', '240', 'JE', 'Jersey', '1', '0.00');
 
 CREATE TABLE `gc_coupons` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
